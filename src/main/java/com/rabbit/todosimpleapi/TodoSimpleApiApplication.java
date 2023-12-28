65b308d33dad8044d8f25456f6a01b7d8fe2adfa
kdukoelho <ce.figueiredoc@gmail.com>
2023-12-28 10:36:43 -0300

    initial commit

---
+++ src/main/java/com/rabbit/todosimpleapi/TodoSimpleApiApplication.java
@@ -0,0 +1,13 @@
+package com.rabbit.todosimpleapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoSimpleApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoSimpleApiApplication.class, args);
+	}
+
+}
