45779349fff921252ea30c30bc6e6ab887255283
kdukoelho <ce.figueiredoc@gmail.com>
2023-12-29 17:43:39 -0300

    fix(TaskRepository): task model import

--- src/main/java/com/rabbit/todosimpleapi/repository/TaskRepository.java
+++ src/main/java/com/rabbit/todosimpleapi/repository/TaskRepository.java
@@ -1,7 +1,7 @@
 package com.rabbit.todosimpleapi.repository;
 
+import com.rabbit.todosimpleapi.model.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.scheduling.config.Task;
 
 public interface TaskRepository extends JpaRepository<Task, String> {
 }
