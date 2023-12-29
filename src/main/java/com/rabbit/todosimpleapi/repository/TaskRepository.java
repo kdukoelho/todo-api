011966a3801586486be2540370fa4de6596c9aa9
kdukoelho <ce.figueiredoc@gmail.com>
2023-12-29 18:06:31 -0300

    feat(repositories): delete user_has_task relationship method

--- src/main/java/com/rabbit/todosimpleapi/repository/TaskRepository.java
+++ src/main/java/com/rabbit/todosimpleapi/repository/TaskRepository.java
@@ -2,6 +2,10 @@ package com.rabbit.todosimpleapi.repository;
 
 import com.rabbit.todosimpleapi.model.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface TaskRepository extends JpaRepository<Task, String> {
+    @Query(value = "DELETE FROM user_has_task WHERE task_id = :task_id", nativeQuery=true)
+    void deleteTaskUserRelationshipById(@Param("task_id") String task_id);
 }
--- src/main/java/com/rabbit/todosimpleapi/repository/UserRepository.java
+++ src/main/java/com/rabbit/todosimpleapi/repository/UserRepository.java
@@ -2,6 +2,10 @@ package com.rabbit.todosimpleapi.repository;
 
 import com.rabbit.todosimpleapi.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface UserRepository extends JpaRepository<User, String> {
+    @Query(value = "DELETE FROM user_has_task WHERE user_id = :user_id;", nativeQuery = true)
+    void deleteUserTaskRelationshipById(@Param("user_id") String user_id);
 }
