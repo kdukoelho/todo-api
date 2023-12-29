525778c23bfc4d853cbeef3ede4a39bd07e1f846
kdukoelho <ce.figueiredoc@gmail.com>
2023-12-29 12:43:27 -0300

    build: custom utf8mb4 dialect to hibernate
    based on hibernate MySQLDialect was created a new dialect that have utf8mb4 as deafult charset and utf8mb4_general_ci as collate

---
+++ src/main/java/com/rabbit/todosimpleapi/build/MySQLDialectUTF8.java
@@ -0,0 +1,16 @@
+package com.rabbit.todosimpleapi.build;
+
+import org.hibernate.dialect.DatabaseVersion;
+import org.hibernate.dialect.MySQLDialect;
+
+public class MySQLDialectUTF8 extends MySQLDialect {
+    private static final int MY_SQL_DIALECT_VERSION_MAJOR = 8;
+
+    public MySQLDialectUTF8() {
+        super(DatabaseVersion.make(MY_SQL_DIALECT_VERSION_MAJOR));
+    }
+    @Override
+    public String getTableTypeString(){
+        return "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci";
+    }
+}
