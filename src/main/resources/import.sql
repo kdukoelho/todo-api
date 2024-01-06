fac514818f6623abcd9e112b80ab2e305822f22e
kdukoelho <ce.figueiredoc@gmail.com>
2024-01-06 10:50:27 -0300

    feat: automatic MySQL population

---
+++ src/main/resources/import.sql
@@ -0,0 +1,32 @@
+INSERT INTO tb_user(id, username, pass_hash) VALUES("692157433776792", "Fernanda Agatha Lima", "o\xcffE\xc3\xaa\x0e}u\xee\t5\xb8\xc2\xa0\x88");
+INSERT INTO tb_user(id, username, pass_hash) VALUES("230928448384961", "Rafaela Yasmin da Rocha", "\x15\xd5\x10\x90\x938\xdb\xe1T\x7f\x9a\xe3\xef\xff\xccV");
+INSERT INTO tb_user(id, username, pass_hash) VALUES("853149405371893", "Guilherme Bento Iago da Silva", "QlY;Iu}\x94:\xe4^\x19\x1e\x02\xe9\xa2");
+INSERT INTO tb_user(id, username, pass_hash) VALUES("204938386570135", "Bárbara Brenda Drumond", "\x88\xe1\xc1L\xf7@\x04xdaA\x16\x8e_qi*");
+INSERT INTO tb_user(id, username, pass_hash) VALUES("188035875437527", "Kaique Manoel Souza", "X\x12\xd6\xa2P]^\xbc\xe6savBO\x80\x91");
+INSERT INTO tb_user(id, username, pass_hash) VALUES("803595792045053", "Tiago Mário Freitas", "7\xa2\x0e\xb7P\xed\xaf\xb9I\x8a\xd91F9VF");
+INSERT INTO tb_user(id, username, pass_hash) VALUES("103257579130023", "Antonella Aurora Aragão", "\xa6\x13\xe9\x0bD\x04\xba\x12f-\xa0\xf9%\x95\x9e\x8e");
+INSERT INTO tb_user(id, username, pass_hash) VALUES("651836329547197", "Theo Luiz Pietro Vieira", "Dr\x86\x10\xa4\xed\xdf\xb8\xc2\x8e\xbf\x0bAin\xe3");
+INSERT INTO tb_user(id, username, pass_hash) VALUES("567478080019408", "Gabriela Regina Baptista", "\xb4.\xbd\xf0\xad\xe0\x810\x9f\xb9\x15B(\xdbO\x85");
+INSERT INTO tb_user(id, username, pass_hash) VALUES("611035770768368", "Thales Daniel Juan Nunes", "9P~\x13@\x15/\x83j\x14\x13\xa1\x84t\xc5");
+
+INSERT INTO tb_task(id, name, description) VALUES("9991706218", "", "");
+INSERT INTO tb_task(id, name, description) VALUES("6705217205", "", "");
+INSERT INTO tb_task(id, name, description) VALUES("5571938128", "", "");
+INSERT INTO tb_task(id, name, description) VALUES("5154610859", "", "");
+INSERT INTO tb_task(id, name, description) VALUES("8946853002", "", "");
+INSERT INTO tb_task(id, name, description) VALUES("9245808801", "", "");
+INSERT INTO tb_task(id, name, description) VALUES("3527105429", "", "");
+INSERT INTO tb_task(id, name, description) VALUES("2896753420", "", "");
+INSERT INTO tb_task(id, name, description) VALUES("4943669304", "", "");
+INSERT INTO tb_task(id, name, description) VALUES("6337744358", "", "");
+
+INSERT INTO user_has_task(user_id, task_id) VALUES("692157433776792", "9991706218");
+INSERT INTO user_has_task(user_id, task_id) VALUES("230928448384961", "6705217205");
+INSERT INTO user_has_task(user_id, task_id) VALUES("853149405371893", "5571938128");
+INSERT INTO user_has_task(user_id, task_id) VALUES("204938386570135", "5154610859");
+INSERT INTO user_has_task(user_id, task_id) VALUES("188035875437527", "8946853002");
+INSERT INTO user_has_task(user_id, task_id) VALUES("803595792045053", "9245808801");
+INSERT INTO user_has_task(user_id, task_id) VALUES("103257579130023", "3527105429");
+INSERT INTO user_has_task(user_id, task_id) VALUES("651836329547197", "2896753420");
+INSERT INTO user_has_task(user_id, task_id) VALUES("567478080019408", "4943669304");
+INSERT INTO user_has_task(user_id, task_id) VALUES("611035770768368", "6337744358");
