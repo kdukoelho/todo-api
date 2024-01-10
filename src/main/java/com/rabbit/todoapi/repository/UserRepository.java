080a36b25ce781f84c53a4dad1bfe5369a7b25ab
kdukoelho <ce.figueiredoc@gmail.com>
2024-01-10 08:37:58 -0300

    feat(Authentication): find by username

--- src/main/java/com/rabbit/todoapi/repository/UserRepository.java
+++ src/main/java/com/rabbit/todoapi/repository/UserRepository.java
@@ -1,11 +1,15 @@
 package com.rabbit.todoapi.repository;
 
-import com.rabbit.todoapi.model.User;
+import com.rabbit.todoapi.model.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.security.core.userdetails.UserDetails;
 
 public interface UserRepository extends JpaRepository<User, String> {
     @Query(value = "DELETE FROM user_has_task WHERE user_id = :user_id;", nativeQuery = true)
     void deleteUserTaskRelationshipById(@Param("user_id") String user_id);
+
+    UserDetails findByUsername(String username);
+
 }
